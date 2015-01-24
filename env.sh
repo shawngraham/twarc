92239e9385e19d08fc61665507c7626d01966351
Shawn Graham <shawn_graham@carleton.ca>
2015-01-24 18:43:02 -0500

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,9 @@
+# replace everything in quotes with what you get from twitter 
+# when you create a new app at https://dev.twitter.com/apps/new
+# then at the prompt type: source env.sh
+# before trying any of the twarc.py commands
+
+export CONSUMER_KEY="CONSUMERKEY"
+export CONSUMER_SECRET="CONSUMERSECRET"
+export ACCESS_TOKEN="ACCESS-TOKEN"
+export ACCESS_TOKEN_SECRET="ACCESSTOKENSECRET"
